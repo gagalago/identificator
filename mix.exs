0190428065e26a08daa7be7cb6c0f04ce0742f48
Claessens Simon <gagalago@gmail.com>
2016-04-02 12:20:56 +0200

    pg version for travis

--- mix.exs
+++ mix.exs
@@ -2,15 +2,17 @@ defmodule Identificator.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :identificator,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     elixirc_paths: elixirc_paths(Mix.env),
-     compilers: [:phoenix, :gettext] ++ Mix.compilers,
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     aliases: aliases,
-     deps: deps]
+    [
+      app: :identificator,
+       version: "0.0.1",
+       elixir: "~> 1.0",
+       elixirc_paths: elixirc_paths(Mix.env),
+       compilers: [:phoenix, :gettext] ++ Mix.compilers,
+       build_embedded: Mix.env == :prod,
+       start_permanent: Mix.env == :prod,
+       aliases: aliases,
+       deps: deps
+    ]
   end
 
   # Configuration for the OTP application.
@@ -58,7 +60,8 @@ defmodule Identificator.Mixfile do
   defp aliases do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"]
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      "test":       ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
