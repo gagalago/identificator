aac046fc94920b5ddf9ab0753e17739995e3579c
Claessens Simon <gagalago@gmail.com>
2016-04-02 12:57:32 +0200

    cleanup

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,6 +4,4 @@ Faker.locale :en
 
 ExUnit.start
 
-# Mix.Task.run "ecto.create", ~w(-r Identificator.Repo --quiet)
-# Mix.Task.run "ecto.migrate", ~w(-r Identificator.Repo --quiet)
 Ecto.Adapters.SQL.Sandbox.mode(Identificator.Repo, :manual)
