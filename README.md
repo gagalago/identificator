80abf1a5b09d38a5b6ffb2e6b50c1884216748af
Claessens Simon <gagalago@gmail.com>
2016-04-24 22:21:54 +0200

    share public key

--- README.md
+++ README.md
@@ -8,5 +8,4 @@ To generate rsa keys
 
 ```elixir
 jwk_rs512 = JOSE.JWK.generate_key({:rsa, 4096})
-JOSE.JWK.to_public(jwk_rs512)
 ```
--- web/views/root_view.ex
+++ web/views/root_view.ex
@@ -2,4 +2,10 @@ defmodule Identificator.RootView do
   @moduledoc false
 
   use Identificator.Web, :view
+
+  def public_key do
+    secret_key = :guardian |> Application.get_env(Guardian) |> Keyword.get(:secret_key)
+    {_module, key} = secret_key.() |> JOSE.JWK.to_public |> JOSE.JWK.to_binary
+    key
+  end
 end
